0dbf11478798c435c082ef23c788b4efb6a2aeee
Daniel Ryu <rujjiryu99@gmail.com>
2022-01-18 00:56:51 -0800

    display dummy data

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,64 @@
 <script lang="ts">
-	export let name: string;
+  import type { Post } from "./lib/types";
+
+  const posts: Post[] = [
+    {
+      title: "test title",
+      description: "test descriptiotniotenioteaw",
+      liked: false,
+      imageCapturedAt: new Date(),
+      imageURL: "https://i.redd.it/0mriegkicyl61.jpg",
+    },
+    {
+      title: "test title 2222",
+      description: "yes",
+      liked: false,
+      imageCapturedAt: new Date(),
+      imageURL: "https://i.redd.it/0mriegkicyl61.jpg",
+    },
+  ];
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Spacestagram</h1>
+  <ul>
+    {#each posts as post, i (post.imageURL + i)}
+      <!-- TODO read this https://svelte.dev/tutorial/keyed-each-blocks -->
+      <li>
+        <div class:post>
+          <p>{post.title}</p>
+          <p>{post.description}</p>
+          <p>{post.liked}</p>
+          <img src={post.imageURL} alt={post.title} />
+        </div>
+      </li>
+    {/each}
+  </ul>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .post > img {
+    width: 100px;
+    height: 100px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ src/lib/types.ts
@@ -0,0 +1,7 @@
+export type Post = {
+  title: string;
+  description: string;
+  liked: boolean;
+  imageURL: string;
+  imageCapturedAt: Date; // TODO earth_date?
+};
